712e1104732db03950b58f5253ec540f1b50f624
allen9535 <allen9535@naver.com>
2023-11-15 09:09:25 +0900

    chore: gunicorn 타임아웃 에러 해결을 위한 script 파일 수정

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 python manage.py collectstatic
 python manage.py makemigrations
 python manage.py migrate
-gunicorn config.wsgi:application --bind 0.0.0.0:8000
+gunicorn config.wsgi:application --bind 0.0.0.0:8000 --timeout 100
